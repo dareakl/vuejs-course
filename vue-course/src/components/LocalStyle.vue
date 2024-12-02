af3c7438c38a4628635fa3b2dbb5022d36abc874
Sathish Raja <msathis.akl@hotmail.com>
2024-12-02 16:03:36 +0800

    local,global,combined,module styles examples

---
+++ vue-course/src/components/LocalStyle.vue
@@ -0,0 +1,3 @@
+<template>
+  <h2 class="text-red">This is Children Component</h2>
+</template>
---
+++ vue-course/src/components/ModuleStyle.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1 :class="myClasses.anotherClass">Using Module CSS with $ syntax (camelVase)</h1>
+</template>
+
+<style module="myClasses">
+.m-class {
+  background: firebrick;
+  color: white;
+}
+.anotherClass {
+  background: seagreen;
+  color: white;
+}
+</style>
