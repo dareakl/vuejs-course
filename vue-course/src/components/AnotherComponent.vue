d5f3ff21c6388cbdb90e250240afbcfbc9d34264
Sathish Raja <msathis.akl@hotmail.com>
2024-12-02 15:26:34 +0800

    text interpolation, render dynamic data examples

--- vue-course/src/components/AnotherComponent.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h1>Nested Component</h1>
-</template>
--- vue-course/src/components/HelloWorld.vue
+++
@@ -1,8 +0,0 @@
-<script setup>
-import AnotherComponent from './AnotherComponent.vue'
-</script>
-
-<template>
-  <h1>This is Parent Component</h1>
-  <AnotherComponent />
-</template>
